d8b68bbc4d4ad1e63d992188dccdd4240df81fa5
hek189 <12451063-hek189@users.noreply.gitlab.com>
2023-03-29 22:08:20 +0200

    limpieza

--- app/src/main/java/com/example/androidchatgpt/Adapter.java
+++ app/src/main/java/com/example/androidchatgpt/Adapter.java
@@ -4,17 +4,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ChatViewHolder> {
     ArrayList<Message> messagesArrayList;
+
     public Adapter(ArrayList<Message> messagesArrayList) {
         this.messagesArrayList = messagesArrayList;
     }
@@ -22,9 +21,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ChatViewHolder> {
     @NonNull
     @Override
     public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View chatView = LayoutInflater.from(parent.getContext()).inflate(R.layout.ai_message, null);
-        ChatViewHolder chatViewHolder = new ChatViewHolder(chatView);
-        return chatViewHolder;
+        View chatView = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_message, parent, false);
+        return new ChatViewHolder(chatView);
     }
 
     /**
@@ -35,11 +33,11 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ChatViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
         Message message = messagesArrayList.get(position);
-        if(message.getSentBy().equals(Message.SENT_HUMAN)){
+        if (message.getSentBy().equals(Message.SENT_HUMAN)) {
             holder.ai_message_Layout.setVisibility(View.GONE);
             holder.human_message_Layout.setVisibility(View.VISIBLE);
             holder.human_message_TextView.setText(message.getMessage());
-        }else{
+        } else {
             holder.human_message_Layout.setVisibility(View.GONE);
             holder.ai_message_Layout.setVisibility(View.VISIBLE);
             holder.ai_message_TextView.setText(message.getMessage());
@@ -47,7 +45,6 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ChatViewHolder> {
     }
 
     /**
-     *
      * @return Total number of messages (Size of array list)
      */
     @Override
@@ -55,9 +52,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ChatViewHolder> {
         return messagesArrayList.size();
     }
 
-    public class ChatViewHolder extends RecyclerView.ViewHolder{
+    public static class ChatViewHolder extends RecyclerView.ViewHolder {
         LinearLayout ai_message_Layout, human_message_Layout;
         TextView ai_message_TextView, human_message_TextView;
+
         public ChatViewHolder(@NonNull View itemView) {
             super(itemView);
             ai_message_Layout = itemView.findViewById(R.id.ai_mssg_layout);
--- app/src/main/java/com/example/androidchatgpt/MainActivity.java
+++ app/src/main/java/com/example/androidchatgpt/MainActivity.java
@@ -26,14 +26,13 @@ import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
     RecyclerView recyclerView;
     EditText message_EditText;
     ImageButton sendButton;
     ArrayList<Message> messageArrayList;
     Adapter adapter;
-    public static final MediaType JSON
-            = MediaType.get("application/json; charset=utf-8");
-
     OkHttpClient client = new OkHttpClient();
 
 
@@ -45,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerview);
         message_EditText = findViewById(R.id.message_edittext);
-        sendButton = findViewById(R.id.enviar);
+        sendButton = findViewById(R.id.send_button);
 
         adapter = new Adapter(messageArrayList);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -54,8 +53,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         sendButton.setOnClickListener((view) -> {
             String message = message_EditText.getText().toString().trim();
-            //Toast.makeText(this,message,Toast.LENGTH_LONG).show();
-            addMessageToChat(message,Message.SENT_HUMAN);
+            addMessageToChat(message, Message.SENT_HUMAN);
             message_EditText.setText("");
             callAPI(message);
         });
@@ -63,10 +61,11 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Adds message to chat view
+     *
      * @param message message to be sent
-     * @param sender if it was sent by human or ChatGPT
+     * @param sender  if it was sent by human or ChatGPT
      */
-    void addMessageToChat(String message, String sender){
+    void addMessageToChat(String message, String sender) {
         runOnUiThread(() -> {
             messageArrayList.add(new Message(message, sender));
             adapter.notifyDataSetChanged();
@@ -76,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Sends message to openAI through a http request by okhttp3
+     * It will return a message with an error if something goes wrong
      * @param message message to send
      */
     void callAPI(String message) {
@@ -83,41 +83,43 @@ public class MainActivity extends AppCompatActivity {
         JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("model", "text-davinci-003");
-            jsonObject.put("prompt",message);
+            jsonObject.put("prompt", message);
             jsonObject.put("max_tokens", 4000);
-            jsonObject.put("temperature",0);
+            jsonObject.put("temperature", 0);
         } catch (JSONException e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
         RequestBody body = RequestBody.create(jsonObject.toString(), JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer "+getString(R.string.apiKey))
+                .header("Authorization", "Bearer " + getString(R.string.apiKey))
                 .post(body)
                 .build();
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                messageArrayList.remove(messageArrayList.size()-1);
-                addMessageToChat(getString(R.string.error)+ e, Message.SENT_GPT);
+                messageArrayList.remove(messageArrayList.size() - 1);
+                addMessageToChat(getString(R.string.error) + e, Message.SENT_GPT);
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     JSONObject jsonObjectResponse;
                     try {
+                        assert response.body() != null;
                         jsonObjectResponse = new JSONObject(response.body().string());
                         JSONArray jsonArray = jsonObjectResponse.getJSONArray("choices");
                         String result = jsonArray.getJSONObject(0).getString("text");
-                        messageArrayList.remove(messageArrayList.size()-1);
+                        messageArrayList.remove(messageArrayList.size() - 1);
                         addMessageToChat(result.trim(), Message.SENT_GPT);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                }else{
-                    messageArrayList.remove(messageArrayList.size()-1);
-                    addMessageToChat(getString(R.string.error)+response.body().toString(), Message.SENT_GPT);
+                } else {
+                    messageArrayList.remove(messageArrayList.size() - 1);
+                    assert response.body() != null;
+                    addMessageToChat(getString(R.string.error) + response.body().toString(), Message.SENT_GPT);
                 }
             }
         });
